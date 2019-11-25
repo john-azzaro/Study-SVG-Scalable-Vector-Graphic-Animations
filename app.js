48c7fc652deb07f82dc4f5886b4c9b86b0f6ced3
John Azzaro <jazzaro@temple.edu>
2019-11-24 22:05:17 -0800

    added loop

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 "use strict";
 
 const logo = document.querySelectorAll('#logo path')                // select the logo path
+                                                                    // console.log(logo) to check if nodelist appears.
+for (let i=0; i < logo.length; i++) {                               // loop through the logo paths...
+  console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);        // "Letter 0 is 493.9128723144531"
+}
