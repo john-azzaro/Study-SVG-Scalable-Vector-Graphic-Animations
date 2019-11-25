ad39dd4023e809601709933691db9ba5052c6ed3
John Azzaro <jazzaro@temple.edu>
2019-11-25 15:03:13 -0800

    updated js, id, notes

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 "use strict";
 
-const logo = document.querySelectorAll('#logo path')                // select the logo path
-                                                                    // console.log(logo) to check if nodelist appears.
-for (let i=0; i < logo.length; i++) {                               // loop through the logo paths...
-  console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);        // "Letter 0 is 493.9128723144531"
+function findLength() {
+  const words = document.querySelectorAll('#words path')                // select the words path
+                                                                        // console.log(words) to check if nodelist appears.
+  for (let i=0; i < words.length; i++) {                                // loop through the words paths...
+    console.log(`Letter ${i} is ${words[i].getTotalLength()}`);         // "Letter 0 is 493.9128723144531"
+  }
 }
+
+function initialize() {
+  findLength();
+}
+
+$(initialize);
+
